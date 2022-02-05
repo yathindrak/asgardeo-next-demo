b0d5e412717259e3847bd4b6186103e73f92ca89
Yathindra <yathindrarawya123@gmail.com>
2022-02-05 14:09:06 +0530

    remove comments

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,7 +8,7 @@ export default NextAuth({
         clientId: process.env.ASGARDEO_CLIENT_ID,
         clientSecret: process.env.ASGARDEO_CLIENT_SECRET,
         type: "oauth",
-        wellKnown: "https://api.asgardeo.io/t/" + process.env.ASGARDEO_ORGANIZATION_NAME + "/oauth2/token/.well-known/openid-configuration",
+        wellKnown: "https://api.asgardeo.io/t/" + process.env.ASGARDEO_ORGANIZATION + "/oauth2/token/.well-known/openid-configuration",
         authorization: { params: { scope: process.env.ASGARDEO_SCOPES } },
         idToken: true,
         checks: ["pkce", "state"],
@@ -22,7 +22,7 @@ export default NextAuth({
                 },
             }
     
-            const response = await fetch("https://api.asgardeo.io/t/" + process.env.ASGARDEO_ORGANIZATION_NAME + "/oauth2/userinfo", config);
+            const response = await fetch("https://api.asgardeo.io/t/" + process.env.ASGARDEO_ORGANIZATION + "/oauth2/userinfo", config);
     
             const userResponse = await response.json();
             
@@ -39,48 +39,6 @@ export default NextAuth({
   
     session: {
       strategy: "jwt",
-    },
-    callbacks: {
-    //   async session({ session, token, user }) {
-    //     if(token?.accessToken) {
-    //         session.accessToken = token.accessToken
-    //         session.idToken = token.idToken
-    
-    //         const config = {
-    //             method: 'GET',
-    //             headers: {
-    //                 'Accept': 'application/json',
-    //                 'Content-Type': 'application/json',
-    //                 'authorization': 'Bearer '+session.accessToken
-    //             },
-    //         }
-    
-    //         const response = await fetch("https://api.asgardeo.io/t/" + process.env.ASGARDEO_ORGANIZATION_NAME + "/oauth2/userinfo", config);
-    
-    //         const userResponse = await response.json();
-            
-    //         const userData = {
-    //             sub: userResponse?.sub,
-    //             name: `${userResponse?.given_name} ${userResponse?.family_name}`.trim(),
-    //             email: userResponse?.email,
-    //             image: userResponse?.profile
-    //         }
-    
-    //         session.user = userData;
-    //     } else {
-    //         session = {};
-    //     }
-  
-    //     return session
-    //   },
-    //   async jwt({ token, user, account, profile, isNewUser }) {
-    //     if (account) {
-    //       token.accessToken = account.access_token
-    //       token.idToken = account.id_token
-    //     }
-  
-    //     return token
-    //   }
     },
   })
   
